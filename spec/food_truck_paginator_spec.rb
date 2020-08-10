a6b2b89b26aa6bd74331520688f343c55c5f0ee4
isalevine <isa.loves.coding@gmail.com>
2020-08-10 00:47:10 -0700

    added empty rspec tests

---
+++ spec/food_truck_paginator_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe FoodTruckPaginator do
+  describe "#call" do
+    context "success" do
+      
+    end
+    context "failure" do
+      
+    end
+  end
+end
